b83bb65b9113c17945cb8c2978a57deb1d3f0b36
Ethan <Ethan6305@icloud.com>
2025-09-21 21:47:07 -0400

    Create secrets.h

---
+++ src/secrets.h
@@ -0,0 +1,9 @@
+#ifndef SECRETS_H
+#define SECRETS_H
+
+const char* TOKEN_SERVER_URL = "https://quincy-garage.vercel.app/api/iot-token";
+const char* IOT_API_KEY      = "your-api-key";
+const char* WEB_API_KEY      = "your-web-key";
+const char* DEVICE_ID        = "esp32-1";
+
+#endif
