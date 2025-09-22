c11392993da12be6e73982a8bd4b9075780357f2
Ethan <Ethan6305@icloud.com>
2025-09-21 21:32:27 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,9 @@
-
+export const firebaseConfig = {
+      apiKey: "AIzaSyARbvYIFIRMZsHjk4E6UoWN7FKmAagO0yU",   // ✅ updated key
+      authDomain: "esp32-garage-controller.firebaseapp.com",
+      databaseURL: "https://esp32-garage-controller-default-rtdb.firebaseio.com",
+      projectId: "esp32-garage-controller",
+      storageBucket: "esp32-garage-controller.appspot.com",
+      messagingSenderId: "390563066349",
+      appId: "1:390563066349:web:79232495e3dfb21631f7e4"
+    };
